281901886d921051c0937ecc10f905bbd680cb2c
Miguel Angel Valverde Lara <93334545+MrValverde@users.noreply.github.com>
2022-12-15 14:56:47 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,36 +3,20 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [{username: "Miguel",
-password: "123456"},
-{username: "Antonio",
-password: "jose"}];
+let users = [];
 
 const isValid = (username)=>{ //returns boolean
-    //write code to check is the username is valid
-        const userMatches = users.filter((user) => user.username === username);
-        return userMatches.length > 0;
-    }
-    
+//write code to check is the username is valid
+}
+
 const authenticatedUser = (username,password)=>{ //returns boolean
-    const matchingUsers = users.filter((user) => user.username === username && user.password === password);
-    return matchingUsers.length > 0;
-      
-    }
+//write code to check if username and password match the one we have in records.
+}
+
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (authenticatedUser(username, password)) {
-      let accessToken = jwt.sign({data:password}, "access", {expiresIn: 3600});
-      req.session.authorization = {accessToken,username};
-      return res.status(200).send("User successfully logged in");
-  }
-  else {
-      return res.status(208).json({message: "Invalid username or password"});
-  }
+  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
